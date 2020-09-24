7afbff4aff2b46b2cda04f99294f8e21f205403a
ikp-773 <abhijithkp773@gmail.com>
2020-09-24 10:34:51 +0530

    commit

--- textSpeech.py
+++ textSpeech.py
@@ -1,7 +1,10 @@
 from gtts import gTTS
 import os
-f=open('test.txt')
-x=f.read()
-audio=gTTS(text=x,lang='en',slow=False)
+
+f = open('test.txt')
+x = f.read()
+
+audio = gTTS(text=x, lang='en', slow=False)
+
 audio.save('test.wav')
-os.system('test.wav')
+os.system('play test.wav')
